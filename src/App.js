e123ae38d11197f2812b3f558fa4219390dbf03b
xergol <sourabhbankey107@gmail.com>
2020-11-16 19:14:18 +0530

    create filter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //feature-1
 import React, { useState } from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'
 
@@ -8,6 +9,31 @@ function App() {
   const [products, setProducts] = useState(data.products);
   const [size, setSize] = useState("");
   const [sort, setSort] = useState("");
+
+  const sortProducts = event => {
+    setSort(event.target.value);
+    const resProducts = products.slice().sort((a,b) => 
+    sort === "lowest"?
+    a.price < b.price ? 1 : -1
+    : sort === "highest" ?
+    a.price > b.price ? 1 : -1
+    : a._id < b._id ? 1 : -1
+    )
+    setProducts(resProducts);
+  }
+  const filterProducts = event => {
+
+    if (event.target.value === "") {
+      setSize(event.target.value);
+      setProducts(data.products);
+    } else {
+      const filteredArray = data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+      setSize(event.target.value);
+      setProducts(filteredArray);
+    }
+
+  }
+
 
 
   return (
@@ -18,7 +44,8 @@ function App() {
       <main>
         <div className="content">
           <div className="main">
-            <Products  products={products}/>
+            <Filter count={products.length} size={size} sort={sort} filterProducts={filterProducts} sortProducts={sortProducts} />
+            <Products products={products} />
           </div>
           <div className="sidebar">
             Cart Items
---
+++ src/components/Filter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Filter(props) {
+    return (
+        <div className="filter">
+            <div className="filter-result">
+                {props.count} Products
+            </div>
+            <div className="filter-sort">
+                Order
+                <select value={props.sort} onChange={props.sortProducts}>
+                    <option>Latest</option>
+                    <option value="lowest">Lowest</option>
+                    <option value="highest">Highest</option>
+                </select>
+            </div>
+            <div className="filter-size">
+                Filter <select value={props.size} onChange={props.filterProducts}>
+                    <option value="">ALL</option>
+                    <option value="XS">XS</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="ML">ML</option>
+                    <option value="XXL">XXL</option>
+                </select>
+            </div>
+        </div>
+    )
+}
